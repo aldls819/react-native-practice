3cb81f5964dfe1a5ce457853cb09afa6ec4ce0c4
aldls819 <aldls819@gmail.com>
2024-04-03 01:14:22 +0900

    :sparkles: Feat: App.tsx 컴포넌트 설정
    Feat: App.tsx 컴포넌트 설정

--- ReactNativeBasic/App.tsx
+++ ReactNativeBasic/App.tsx
@@ -17,102 +17,61 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const test = '123';
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1}}>
+        <ScrollView>
+          <View
+            style={{
+              height: 2000,
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'flex-start',
+            }}>
+            <Text
+              allowFontScaling={false}
+              style={{fontSize: 20, fontWeight: 'bold'}}>
+              안녕하세요
+            </Text>
+            <Text
+              allowFontScaling={false}
+              style={{fontSize: 20, fontWeight: 'bold'}}>
+              반갑습니다
+            </Text>
+            <Text
+              allowFontScaling={false}
+              style={{fontSize: 20, fontWeight: 'bold'}}>
+              이것은 리액트네이티브
+            </Text>
+          </View>
+          <View style={{backgroundColor: 'blue'}}>
+            <Text
+              allowFontScaling={false}
+              ellipsizeMode="tail"
+              numberOfLines={1}
+              style={{color: '#fff', fontSize: 20, fontWeight: 'bold'}}>
+              반갑습니다 {test} 반갑습니다 {test}반갑습니다 {test}반갑습니다{' '}
+              {test}
+              반갑습니다 {test}반갑습니다 {test}반갑습니다 {test}반갑습니다{' '}
+              {test}
+            </Text>
+          </View>
+          <ScrollView showsHorizontalScrollIndicator={false} horizontal>
+            <Text>가로 스크롤 뷰</Text>
+            <Text>가로 스크롤 뷰</Text>
+            <Text>가로 스크롤 뷰</Text>
+            <Text>가로 스크롤 뷰</Text>
+            <Text>가로 스크롤 뷰</Text>
+          </ScrollView>
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
